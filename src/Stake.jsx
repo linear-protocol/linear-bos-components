e982341c81b2eacf4f7bb52c94a3744d21dde686
keeplearning <steve@lit.tech>
2023-04-13 10:06:59 +0800

    fix: put the BrandImage directly into the component

--- src/Stake.jsx
+++ src/Stake.jsx
@@ -261,27 +261,23 @@ const NEARInput = ({ value, onChange, onClickMax }) => {
   );
 };
 
-const BrandImage = () => (
-  <a href="https://linearprotocol.org/" target="_blank">
-    <img
-      style={{
-        height: "20px",
-        width: "auto",
-        position: "absolute",
-        left: "32px",
-        top: "44px",
-      }}
-      src="https://ipfs.near.social/ipfs/bafkreifb45onycd5nycpvt6vboe54zc5c4lynjg5xare4i2tqblwlkogoq"
-      alt="Brand Logo"
-      height={20}
-      width={auto}
-    />
-  </a>
-);
-
 return (
   <Main>
-    <BrandImage />
+    <a href="https://linearprotocol.org/" target="_blank">
+      <img
+        style={{
+          height: "20px",
+          width: "auto",
+          position: "absolute",
+          left: "32px",
+          top: "44px",
+        }}
+        src="https://ipfs.near.social/ipfs/bafkreifb45onycd5nycpvt6vboe54zc5c4lynjg5xare4i2tqblwlkogoq"
+        alt="Brand Logo"
+        height={20}
+        width={auto}
+      />
+    </a>
     <Title>Stake Your NEAR</Title>
     <Description>
       Stake NEAR and receive LiNEAR while earning staking rewards
