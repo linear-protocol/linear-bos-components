b784ea7fa7cd1f72ef52f3b3437974dea95548e2
keeplearning <steve@lit.tech>
2023-04-26 16:09:38 +0800

    fix: remove 2 return

--- src/BrandLogo.jsx
+++ src/BrandLogo.jsx
@@ -15,20 +15,3 @@ return (
     />
   </a>
 );
-return (
-  <a href="https://linearprotocol.org/" target="_blank">
-    <img
-      style={{
-        height: "20px",
-        width: "auto",
-        position: "absolute",
-        left: "32px",
-        top: "44px",
-      }}
-      src="https://ipfs.near.social/ipfs/bafkreifb45onycd5nycpvt6vboe54zc5c4lynjg5xare4i2tqblwlkogoq"
-      alt="Brand Logo"
-      height={20}
-      width={"auto"}
-    />
-  </a>
-);
