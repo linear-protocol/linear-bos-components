30931f145abc69b868a809b09ac0b712c5cd9e07
Linguists <95207870+linguists@users.noreply.github.com>
2023-04-27 16:17:30 +0800

    chore: remove eslint

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,10 +1,5 @@
 export default {
-  "src/**/*.ts?(x)": [
-    () => "tsc --project tsconfig.json --alwaysStrict",
-    "yarn test --watchAll=false --passWithNoTests --bail --findRelatedTests",
-  ],
   "src/**/*.{js,jsx,ts,tsx,md,html,css}": [
     "prettier --write",
-    "eslint --fix --max-warnings 0",
   ],
 };
